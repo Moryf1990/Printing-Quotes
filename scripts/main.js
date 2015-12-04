f6a5ad89e378a1641da296f695b84ed8dd7c2c35
Mory Friedman <friedmanmory2@gmail.com>
2015-12-04 01:12:26 -0600

    Printing Quotes

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var button = $('#button');
+var button1 = $('#button1');
+
+button.on('click', function() {
+	button = prompt('What is the quote?', 'Enter Your Quote Here');
+	button1 = prompt('Who said it?');
+
+	if(button !==null) {
+		console.log(button1 + ' says, ' +  button)
+	}
+});
