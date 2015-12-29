d67f59968c625f31ec7cf9e1d5e99cbdba812d80
Mory Friedman <friedmanmory2@gmail.com>
2015-12-29 13:22:01 -0600

    making console.log work properly

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -4,7 +4,8 @@
 var button = $('#button');
 var button1 = $('#button1');
 
-button.on('click', function () {
+button.on('click', function (e) {
+	e.preventDefault();
 	button = prompt('What is the quote?', 'Enter Your Quote Here');
 	button1 = prompt('Who said it?');
 
--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,8 @@
 var button = $('#button');
 var button1 = $('#button1');
 
-button.on('click', function() {
+button.on('click', function(e) {
+	e.preventDefault();
 	button = prompt('What is the quote?', 'Enter Your Quote Here');
 	button1 = prompt('Who said it?');
 
